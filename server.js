0ddfc4557b2c9fdaa87ab05e85d63fc838790baf
Christy Fury <christy.fury@gmail.com>
2023-03-16 22:11:25 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -9,7 +9,26 @@ app.get("/greeting/:name", (req, res) =>{
     res.send("Hello there " + name)
 })
 
+app.get("/tip/:total/:percentage", (req, res) =>{
+    const total = req.params.total
+    const tipPercent =  req.params.percentage/100
+    const tipAmount = total * tipPercent
+
+    res.send("your tip amount is $" + tipAmount)
+})
+
+app.get("/magic/:question", (req, res) =>{
+    const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    const answer = 
+    res.send()
+})
+
+// listening...
 app.listen(PORT, ()=>{
     console.log("listening on port 3000")
 } );
 
+// app.get("/", (req, res) =>{
+    
+//     res.send()
+// })
