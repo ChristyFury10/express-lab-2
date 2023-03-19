e670fb2738b429e41a8fda099e76be82a7e71b01
Christy Fury <christy.fury@gmail.com>
2023-03-18 20:47:02 -0400

    completes required deliverable lab

--- server.js
+++ server.js
@@ -21,14 +21,28 @@ app.get("/magic/:question", (req, res) =>{
     const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
     const answer = Math.floor(Math.random() * 20);
     const response = responses[answer]
-        res.send(`
-            <html>
-                <body>
-                    <h1>The answer to your question is: ${response}</h1>
-                </body>
-            </html>
-        `)
+        res.send(`<h1>The answer to your question is: ${response} </h1>`)
     })
+
+//************ beer countdown app******************//
+
+app.get("/:beers", (req, res) =>{
+    const beer = parseInt(req.params.beers)
+    if(beer >= 1){
+        let num = beer -1;
+res.send(`${beer} bottles of beer on the wall <a href="http://localhost:3000/${num}">Take one down...</a>`)
+}
+// else if (beers = 1){
+//     let num = beer -1;
+//     res.send(`One beer left onthe wall! <a href="http://localhost:3000/${num}">Take one down...</a>`)
+// }
+else {
+    res.send(`youre out of beer <a href = "/99">play again?</a>`)
+}
+
+})
+
+
 
 // listening...
 app.listen(PORT, ()=>{
